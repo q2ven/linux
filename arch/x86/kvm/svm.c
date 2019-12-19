24e3fbb5efd3333cc936c1fa676ee2fbe2d3b48b
John Allen <john.allen@amd.com>
2019-12-19 14:17:59 -0600

    kvm/svm: PKU not currently supported
    Current SVM implementation does not have support for handling PKU. Guests
    running on a host with future AMD cpus that support the feature will read
    garbage from the PKRU register and will hit segmentation faults on boot as
    memory is getting marked as protected that should not be. Ensure that cpuid
    from SVM does not advertise the feature.
    
    Signed-off-by: John Allen <john.allen@amd.com>
    Cc: stable@vger.kernel.org
    Fixes: 0556cbdc2fbc ("x86/pkeys: Don't check if PKRU is zero before writing it")
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit a47970ed74a535b1accb4bc73643fd5a93993c3e)
    [Contextual changes, pre do_cpuid_7_mask() and arch/x86/kvm/vmx/]
    Signed-off-by: Samuel Mendoza-Jonas <samjonas@amazon.com>

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -5486,6 +5486,11 @@ static bool svm_has_wbinvd_exit(void)
 	return true;
 }
 
+static bool svm_pku_supported(void)
+{
+	return false;
+}
+
 #define PRE_EX(exit)  { .exit_code = (exit), \
 			.stage = X86_ICPT_PRE_EXCEPT, }
 #define POST_EX(exit) { .exit_code = (exit), \
@@ -5793,6 +5798,7 @@ static struct kvm_x86_ops svm_x86_ops __ro_after_init = {
 	.invpcid_supported = svm_invpcid_supported,
 	.mpx_supported = svm_mpx_supported,
 	.xsaves_supported = svm_xsaves_supported,
+	.pku_supported = svm_pku_supported,
 
 	.set_supported_cpuid = svm_set_supported_cpuid,
 
