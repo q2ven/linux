e4cc07baba38f33365e4dd3c8d8f1c6daa2888e9
Samuel Mendoza-Jonas <samjonas@amazon.com>
2020-12-11 21:20:33 +0000

    Revert "XEN uses irqdesc::irq_data_common::handler_data to store a per interrupt XEN data pointer which contains XEN specific information."
    This reverts commit 881947cde19a6a823441f20a1ca3d45ba74c5dfc.
    
    Signed-off-by: Samuel Mendoza-Jonas <samjonas@amazon.com>

--- drivers/xen/events/events_base.c
+++ drivers/xen/events/events_base.c
@@ -191,7 +191,7 @@ int get_evtchn_to_irq(unsigned evtchn)
 /* Get info for IRQ */
 struct irq_info *info_for_irq(unsigned irq)
 {
-	return irq_get_chip_data(irq);
+	return irq_get_handler_data(irq);
 }
 
 static void delayed_free_irq(struct work_struct *work)
@@ -632,7 +632,7 @@ static void xen_irq_init(unsigned irq)
 	info->refcnt = -1;
 	INIT_WORK(&info->work, delayed_free_irq);
 
-	irq_set_chip_data(irq, info);
+	irq_set_handler_data(irq, info);
 
 	INIT_LIST_HEAD(&info->eoi_list);
 	list_add_tail(&info->list, &xen_irq_list_head);
@@ -682,7 +682,7 @@ static int __must_check xen_allocate_irq_gsi(unsigned gsi)
 
 static void xen_free_irq(unsigned irq)
 {
-	struct irq_info *info = irq_get_chip_data(irq);
+	struct irq_info *info = irq_get_handler_data(irq);
 
 	if (WARN_ON(!info))
 		return;
@@ -861,7 +861,7 @@ EXPORT_SYMBOL_GPL(xen_irq_from_gsi);
 static void __unbind_from_irq(unsigned int irq)
 {
 	int evtchn = evtchn_from_irq(irq);
-	struct irq_info *info = irq_get_chip_data(irq);
+	struct irq_info *info = irq_get_handler_data(irq);
 
 	if (info->refcnt > 0) {
 		info->refcnt--;
@@ -1439,7 +1439,7 @@ int bind_ipi_to_irqhandler(enum ipi_vector ipi,
 
 void unbind_from_irqhandler(unsigned int irq, void *dev_id)
 {
-	struct irq_info *info = irq_get_chip_data(irq);
+	struct irq_info *info = irq_get_handler_data(irq);
 
 	if (WARN_ON(!info))
 		return;
@@ -1473,7 +1473,7 @@ int evtchn_make_refcounted(unsigned int evtchn)
 	if (irq == -1)
 		return -ENOENT;
 
-	info = irq_get_chip_data(irq);
+	info = irq_get_handler_data(irq);
 
 	if (!info)
 		return -ENOENT;
@@ -1501,7 +1501,7 @@ int evtchn_get(unsigned int evtchn)
 	if (irq == -1)
 		goto done;
 
-	info = irq_get_chip_data(irq);
+	info = irq_get_handler_data(irq);
 
 	if (!info)
 		goto done;
