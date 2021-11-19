1b299f8dab26a078f50c64171a9f8cbef4793a48
Poorva Sonparote <psonparo@redhat.com>
2021-11-19 12:41:34 -0800

    ipv4: Exposing __ip_sock_set_tos() in ip.h
    commit 4f47d5d507d6f211ebceac76a5f0b83c2eae154b upstream.
    
    Making the static function __ip_sock_set_tos() from net/ipv4/ip_sockglue.c
    accessible by declaring it in include/net/ip.h
    The reason for doing this is to use this function to set IP_TOS value in
    mptcp_setsockopt() without the lock.
    
    Signed-off-by: Poorva Sonparote <psonparo@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4f47d5d507d6f211ebceac76a5f0b83c2eae154b)

--- net/ipv4/ip_sockglue.c
+++ net/ipv4/ip_sockglue.c
@@ -583,7 +583,7 @@ out:
 	return err;
 }
 
-static void __ip_sock_set_tos(struct sock *sk, int val)
+void __ip_sock_set_tos(struct sock *sk, int val)
 {
 	if (sk->sk_type == SOCK_STREAM) {
 		val &= ~INET_ECN_MASK;
