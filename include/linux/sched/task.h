c35dedacdfcda79c5e8ae29c4ee925f0693cc424
Oleg Nesterov <oleg@redhat.com>
2022-07-11 18:16:25 +0200

    fix race between exit_itimers() and /proc/pid/timers
    As Chris explains, the comment above exit_itimers() is not correct,
    we can race with proc_timers_seq_ops. Change exit_itimers() to clear
    signal->posix_timers with ->siglock held.
    
    Cc: <stable@vger.kernel.org>
    Reported-by: chris@accessvector.net
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Samuel Mendoza-Jonas <samjonas@amazon.com>

--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -72,7 +72,7 @@ static inline void exit_thread(struct task_struct *tsk)
 extern void do_group_exit(int);
 
 extern void exit_files(struct task_struct *);
-extern void exit_itimers(struct signal_struct *);
+extern void exit_itimers(struct task_struct *);
 
 extern long _do_fork(unsigned long, unsigned long, unsigned long, int __user *, int __user *, unsigned long);
 extern long do_fork(unsigned long, unsigned long, unsigned long, int __user *, int __user *);
