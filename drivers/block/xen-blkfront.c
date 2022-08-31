b484a9a27224807c2bcb4924f5976bc45ccc10c1
SeongJae Park <sj@kernel.org>
2022-08-31 16:58:23 +0000

    xen-blkfront: Advertise feature-persistent as user requested
    commit 9f5e0fe5d05f7e8de7f39b2b10089834eb0ff787 upstream.
    
    The advertisement of the persistent grants feature (writing
    'feature-persistent' to xenbus) should mean not the decision for using
    the feature but only the availability of the feature.  However, commit
    74a852479c68 ("xen-blkfront: add a parameter for disabling of persistent
    grants") made a field of blkfront, which was a place for saving only the
    negotiation result, to be used for yet another purpose: caching of the
    'feature_persistent' parameter value.  As a result, the advertisement,
    which should follow only the parameter value, becomes inconsistent.
    
    This commit fixes the misuse of the semantic by making blkfront saves
    the parameter value in a separate place and advertises the support based
    on only the saved value.
    
    Fixes: 74a852479c68 ("xen-blkfront: add a parameter for disabling of persistent grants")
    Cc: <stable@vger.kernel.org> # 5.10.x
    Suggested-by: Juergen Gross <jgross@suse.com>
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Tested-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Reviewed-by: Juergen Gross <jgross@suse.com>
    Link: https://lore.kernel.org/r/20220831165824.94815-3-sj@kernel.org
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- drivers/block/xen-blkfront.c
+++ drivers/block/xen-blkfront.c
@@ -234,6 +234,9 @@ struct blkfront_info
 	unsigned int feature_fua:1;
 	unsigned int feature_discard:1;
 	unsigned int feature_secdiscard:1;
+	/* Connect-time cached feature_persistent parameter */
+	unsigned int feature_persistent_parm:1;
+	/* Persistent grants feature negotiation result */
 	unsigned int feature_persistent:1;
 	unsigned int bounce:1;
 	unsigned int discard_granularity;
@@ -2098,7 +2101,7 @@ again:
 
 	err = xenbus_printf(xbt, dev->nodename,
 			    "feature-persistent", "%u",
-			    info->feature_persistent);
+			    info->feature_persistent_parm);
 	if (err)
 		dev_warn(&dev->dev,
 			 "writing persistent grants feature to xenbus");
@@ -2590,7 +2593,8 @@ static void blkfront_gather_backend_features(struct blkfront_info *info)
 	if (xenbus_read_unsigned(info->xbdev->otherend, "feature-discard", 0))
 		blkfront_setup_discard(info);
 
-	if (xen_blkif_feature_persistent)
+	info->feature_persistent_parm = xen_blkif_feature_persistent;
+	if (info->feature_persistent_parm)
 		info->feature_persistent =
 			!!xenbus_read_unsigned(info->xbdev->otherend,
 					       "feature-persistent", 0);
