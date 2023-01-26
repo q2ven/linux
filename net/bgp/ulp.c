82c0e09dd75a4e26a01ae01440180b89bd2de640
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-01-26 02:29:09 +0000

    bgp: Add BGP ULP.
    We can set BGP ULP by setsockopt(SOL_TCP, TCP_ULP, "bgp", ...) after
    a socket established a TCP connection.
    
      python example:
    
      >>> TCP_ULP = 31
      >>> sk.setsockopt(SOL_TCP, TCP_ULP, b"bgp")
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/bgp/ulp.c
+++ net/bgp/ulp.c
@@ -2,14 +2,35 @@
 /* Copyright Amazon.com Inc. or its affiliates. */
 
 #include <linux/module.h>
+#include <net/sock.h>
+#include <net/tcp.h>
+
+static int bgp_ulp_init(struct sock *sk)
+{
+	sock_owned_by_me(sk);
+
+	if (sk->sk_state != TCP_ESTABLISHED)
+		return -ENOTCONN;
+
+	return 0;
+}
+
+static struct tcp_ulp_ops bgp_ulp_ops __read_mostly = {
+	.name		= "bgp",
+	.owner		= THIS_MODULE,
+	.init		= bgp_ulp_init,
+};
 
 static int __init bgp_init(void)
 {
+	tcp_register_ulp(&bgp_ulp_ops);
+
 	return 0;
 }
 
 static void __exit bgp_exit(void)
 {
+	tcp_unregister_ulp(&bgp_ulp_ops);
 }
 
 module_init(bgp_init);
@@ -18,3 +39,4 @@ module_exit(bgp_exit);
 MODULE_AUTHOR("Kuniyuki Iwashima");
 MODULE_DESCRIPTION("Border Gateway Protocol");
 MODULE_LICENSE("GPL");
+MODULE_ALIAS_TCP_ULP("bgp");
