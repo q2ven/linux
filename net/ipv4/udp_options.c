7e43c9910ea5cd78326d0581c338d80031318dc4
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-07-04 22:04:27 +0000

    udp: opt: Parse zero padding before OCS.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/udp_options.c
+++ net/ipv4/udp_options.c
@@ -4,8 +4,26 @@
 
 int udp_parse_options(struct sk_buff *skb)
 {
+	const unsigned char *ptr;
+	struct udphdr *uh;
+	int left;
+
 	if (!pskb_may_pull(skb, skb->len))
 		return -ENOMEM;
 
+	uh = udp_hdr(skb);
+	ptr = skb->data + ntohs(uh->len);
+	left = skb->len - ntohs(uh->len);
+
+	if ((unsigned long)ptr & 1) {
+		/* Must be zero.  Section 6. */
+		if (*ptr != 0)
+			goto skip;
+
+		ptr++;
+		left--;
+	}
+
+skip:
 	return 0;
 }
