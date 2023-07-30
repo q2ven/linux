48d3c5018b5bbc614072456c926ed81d9f6ddc7c
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-07-30 00:06:21 +0000

    tcp: Support [sg]etsockopt(TCP_EXT_DATA_OFFSET).
    We can enable/disable EDO on a per-socket basis with setsockopt().
    
    The attribute can be changed only before the connection is initiated,
    meaning TCP_CLOSE for active open connection and TCP_LISTEN for passive
    open one.
    
      $ python3
      >>> from socket import *
      >>>
      >>> TCP_EXT_DATA_OFFSET = 38
      >>> TCP_EDO_HDR = 1
      >>>
      >>> s = socket()
      >>> s.setsockopt(SOL_TCP, TCP_EXT_DATA_OFFSET, TCP_EDO_HDR)
    
    The listener's attribute will be inherited to passive open connections.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3668,6 +3668,28 @@ int do_tcp_setsockopt(struct sock *sk, int level, int optname,
 			tcp_enable_tx_delay();
 		WRITE_ONCE(tp->tcp_tx_delay, val);
 		break;
+	case TCP_EXT_DATA_OFFSET:
+		if ((1 << sk->sk_state) & ~(TCPF_CLOSE | TCPF_LISTEN)) {
+			switch (val) {
+			case TCP_EDO_HDR_SEG:
+				tp->edo = 1;
+				tp->edo_seg = 1;
+				break;
+			case TCP_EDO_HDR:
+				tp->edo = 1;
+				tp->edo_seg = 0;
+				break;
+			case TCP_EDO_OFF:
+				tp->edo = 0;
+				tp->edo_seg = 0;
+				break;
+			default:
+				err = -EINVAL;
+			}
+		} else {
+			err = -EINVAL;
+		}
+		break;
 	default:
 		err = -ENOPROTOOPT;
 		break;
@@ -4148,6 +4170,12 @@ int do_tcp_getsockopt(struct sock *sk, int level,
 
 	case TCP_TX_DELAY:
 		val = READ_ONCE(tp->tcp_tx_delay);
+		break;
+
+	case TCP_EXT_DATA_OFFSET:
+		val = tp->edo;
+		if (val)
+			val += tp->edo_seg;
 		break;
 
 	case TCP_TIMESTAMP:
