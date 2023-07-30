47ff536f728a593086514a33460f36d51828bdde
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-07-30 00:06:21 +0000

    tcp: Support [sg]etsockopt(TCP_EXT_DATA_OFFSET).
    We can enable/disable EDO on a per-socket basis with setsockopt().
    
    The attribute can be changed only before the connection is initiated,
    meaning TCP_CLOSE for active open connection and TCP_LISTEN for passive
    open one.
    
      $ python3
      >>> from socket import *
      >>>
      >>> TCP_EXT_DATA_OFFSET = 44
      >>> TCP_EDO_HDR = 1
      >>>
      >>> s = socket()
      >>> s.setsockopt(SOL_TCP, TCP_EXT_DATA_OFFSET, TCP_EDO_HDR)
    
    The listener's attribute will be inherited to passive open connections.
    
    Note that MD5 and TCP-AO cannot be used with EDO for now but will be
    supported.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -3677,7 +3677,10 @@ int do_tcp_setsockopt(struct sock *sk, int level, int optname,
 			goto ao_parse;
 		if (tp->repair)
 			goto ao_parse;
-		err = -EISCONN;
+		if (tp->edo)
+			err = -EOPNOTSUPP;
+		else
+			err = -EISCONN;
 		break;
 ao_parse:
 		err = tp->af_specific->ao_parse(sk, optname, optval, optlen);
@@ -3687,7 +3690,10 @@ ao_parse:
 #ifdef CONFIG_TCP_MD5SIG
 	case TCP_MD5SIG:
 	case TCP_MD5SIG_EXT:
-		err = tp->af_specific->md5_parse(sk, optname, optval, optlen);
+		if (tp->edo)
+			err = -EOPNOTSUPP;
+		else
+			err = tp->af_specific->md5_parse(sk, optname, optval, optlen);
 		break;
 #endif
 	case TCP_FASTOPEN:
@@ -3751,6 +3757,33 @@ ao_parse:
 			tcp_enable_tx_delay();
 		WRITE_ONCE(tp->tcp_tx_delay, val);
 		break;
+	case TCP_EXT_DATA_OFFSET: {
+		struct tcp_key key;
+
+		if (val < TCP_EDO_OFF || val > TCP_EDO_HDR_SEG) {
+			err = -EINVAL;
+			break;
+		}
+
+		if ((1 << sk->sk_state) & ~(TCPF_CLOSE | TCPF_LISTEN)) {
+			err = -EISCONN;
+			break;
+		}
+
+		if (sk_is_mptcp(sk)) {
+			err = -EOPNOTSUPP;
+			break;
+		}
+
+		tcp_get_current_key(sk, &key);
+		if (!tcp_key_is_none(&key)) {
+			err = -EOPNOTSUPP;
+			break;
+		}
+
+		tp->edo = val;
+		break;
+	}
 	default:
 		err = -ENOPROTOOPT;
 		break;
@@ -4241,6 +4274,10 @@ int do_tcp_getsockopt(struct sock *sk, int level,
 
 	case TCP_TX_DELAY:
 		val = READ_ONCE(tp->tcp_tx_delay);
+		break;
+
+	case TCP_EXT_DATA_OFFSET:
+		val = tp->edo;
 		break;
 
 	case TCP_TIMESTAMP:
