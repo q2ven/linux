87a08960ffd2180f0b23dbc55134a875e9c19975
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-07-30 01:12:30 +0000

    tcp: Add EDO-Supported option in SYN.
    Now the EDO-Supported option occupies 32 bits (0101 fc02 below) but
    should be put in a NOPs hole.
    
      07:54:43.401151 IP localhost.41904 > localhost.35101:
        Flags [S], seq 1571701834:1571701836, win 33280,
        options [nop,nop,unknown-252,mss 65495,sackOK,TS val 1802299903 ecr 0], length 2
    
    	0x0000:  4500 003e be28 4000 4006 7e8f 7f00 0001
    	0x0010:  7f00 0001 a3b0 891d 5dae 444a 0000 0000
    	0x0020:  a002 8200 fe32 0000 0101 fc02 0204 ffd7
    	0x0030:  0402 080a 6b6c e9ff 0000 0000 0103
    
    Also, there should be a good threshold to enable/disable EDO, like SACK
    or MPTCP is enabled.  This should be determined by experts.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -187,6 +187,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOPT_MD5SIG		19	/* MD5 Signature (RFC2385) */
 #define TCPOPT_MPTCP		30	/* Multipath TCP (RFC6824) */
 #define TCPOPT_FASTOPEN		34	/* Fast open (RFC7413) */
+#define TCPOPT_EDO_SUPPORTED	252	/* Extended Data Offset Supported */
 #define TCPOPT_EXP		254	/* Experimental */
 /* Magic number to be after the option value for sharing TCP
  * experimental options. See draft-ietf-tcpm-experimental-options-00.txt
@@ -217,6 +218,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCPOLEN_MD5SIG_ALIGNED		20
 #define TCPOLEN_MSS_ALIGNED		4
 #define TCPOLEN_EXP_SMC_BASE_ALIGNED	8
+#define TCPOLEN_EDO_SUPPORTED		2
 
 /* Flags in tp->nonagle */
 #define TCP_NAGLE_OFF		1	/* Nagle's algo is disabled */
--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -415,6 +415,7 @@ static inline bool tcp_urg_mode(const struct tcp_sock *tp)
 #define OPTION_FAST_OPEN_COOKIE	BIT(8)
 #define OPTION_SMC		BIT(9)
 #define OPTION_MPTCP		BIT(10)
+#define OPTION_EDO_SUPPORTED	BIT(11)
 
 static void smc_options_write(__be32 *ptr, u16 *options)
 {
@@ -612,6 +613,12 @@ static void tcp_options_write(struct tcphdr *th, struct tcp_sock *tp,
 	__be32 *ptr = (__be32 *)(th + 1);
 	u16 options = opts->options;	/* mungable copy */
 
+	if (unlikely(OPTION_EDO_SUPPORTED & options)) {
+		/* TODO: put in a NOPs hole. */
+		*ptr++ = htonl((TCPOPT_NOP << 24) | (TCPOPT_NOP << 16) |
+			       (TCPOPT_EDO_SUPPORTED << 8) | TCPOLEN_EDO_SUPPORTED);
+	}
+
 	if (unlikely(OPTION_MD5 & options)) {
 		*ptr++ = htonl((TCPOPT_NOP << 24) | (TCPOPT_NOP << 16) |
 			       (TCPOPT_MD5SIG << 8) | TCPOLEN_MD5SIG);
@@ -789,6 +796,11 @@ static unsigned int tcp_syn_options(struct sock *sk, struct sk_buff *skb,
 	 * going out.  */
 	opts->mss = tcp_advertise_mss(sk);
 	remaining -= TCPOLEN_MSS_ALIGNED;
+
+	if (tp->ext_doff) {
+		opts->options |= OPTION_EDO_SUPPORTED;
+		remaining -= (TCPOLEN_EDO_SUPPORTED + 2);
+	}
 
 	if (likely(READ_ONCE(sock_net(sk)->ipv4.sysctl_tcp_timestamps) && !*md5)) {
 		opts->options |= OPTION_TS;
