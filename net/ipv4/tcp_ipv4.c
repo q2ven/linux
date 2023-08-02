8bba87c23593cf28e5f0a0d0e6e63053da313c0a
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-08-02 04:13:49 +0000

    tcp: Disable fast paths for EDO packets.
    We need to parse all TCP options for EDO-enabled sockets.  If a packet
    does not have EDO Extension option, we have to drop it.
    
       5.2. EDO Extension
    
       >> Once enabled on a connection, all segments in both directions
       MUST include the EDO Extension option.
    
       >> If EDO has been negotiated, any subsequent segments arriving
       without the EDO Extension option MUST be silently ignored.
    
    If we coalesced packets in tcp_add_backlog() or went through the fast
    path in tcp_rcv_established(), the extra options would be ACKed as user
    data and recv()ed by application.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -2048,7 +2048,7 @@ bool tcp_add_backlog(struct sock *sk, struct sk_buff *skb,
 	    tail->decrypted != skb->decrypted ||
 #endif
 	    !mptcp_skb_can_collapse(tail, skb) ||
-	    thtail->doff != th->doff ||
+	    thtail->doff != th->doff || tcp_sk(sk)->edo ||
 	    memcmp(thtail + 1, th + 1, hdrlen - sizeof(*th)))
 		goto no_coalesce;
 
