3a2da83efe0d36be98a2dfd9e25a9d65d3bf81fd
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-08-04 19:14:49 +0000

    tcp: Prepare for EDO in tcp_parse_options().
    We will add parser for EDO Extension option.
    
    When processing incoming TCP packets, we pull (th->doff * 4) bytes
    into the linear area of skb in tcp_v4_rcv().
    
    However, this might not be enough in case of EDO, and we could
    end up calling pskb_may_pull() for the extra options.
    
    So, we need to drop const from skb to call pskb_may_pull() and
    return error if it fails.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -430,9 +430,9 @@ void tcp_data_ready(struct sock *sk);
 int tcp_mmap(struct file *file, struct socket *sock,
 	     struct vm_area_struct *vma);
 #endif
-void tcp_parse_options(const struct net *net, const struct sk_buff *skb,
-		       struct tcp_options_received *opt_rx,
-		       int estab, struct tcp_fastopen_cookie *foc);
+int tcp_parse_options(const struct net *net, struct sk_buff *skb,
+		      struct tcp_options_received *opt_rx,
+		      int estab, struct tcp_fastopen_cookie *foc);
 const u8 *tcp_parse_md5sig_option(const struct tcphdr *th);
 
 /*
