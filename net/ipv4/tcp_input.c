2e054b61c7f0d3d789234168bc7b913dbcb99352
Kuniyuki Iwashima <kuniyu@amazon.com>
2023-08-07 17:15:53 +0000

    tcp: Add parser for EDO Extension option.
    We add a new argument parse_edo_ext in tcp_parse_options() to control
    whether EDO Extension option is parsed or not.  Now it is disabled in
    all paths and never fails.  We will turn it on later for each path.
    
    If a segment has EDO Extension option, we call pskb_may_pull() and
    update some values used in tcp_parse_options().  Then, we also need
    to update TCP_SKB_CB(skb)->end_seq.
    
    TCP_SKB_CB(skb)->end_seq is filled in tcp_v[46]_fill_cb() as
    
      TCP_SKB_CB(skb)->seq + th->syn + th->fin + skb->len - th->doff * 4
    
    and set to tp->rcv_nxt in tcp_rcv_nxt_update() to use the value as
    ACK number.
    
    If we forgot to update end_seq, the extended area placed after 60 bytes
    would be treated as user data and a wrong ACK would be sent for it.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4135,13 +4135,55 @@ u16 tcp_parse_mss_option(const struct tcphdr *th, u16 user_mss)
 }
 EXPORT_SYMBOL_GPL(tcp_parse_mss_option);
 
+static int tcp_parse_edo_extension(struct sk_buff *skb, const struct tcphdr **thp,
+				   const unsigned char **ptr, int *left, int opsize)
+{
+	u16 hdr_len;
+	u8 parsed;
+
+	if (opsize == TCPOLEN_EXP_EDO_EXT_SEG) {
+		u16 seg_len = get_unaligned_be16(*ptr + 4);
+
+		if (seg_len != skb->len)
+			return -EINVAL;
+	} else if (opsize != TCPOLEN_EXP_EDO_EXT_HDR) {
+		return -EINVAL;
+	}
+
+	hdr_len = get_unaligned_be16(*ptr + 2);
+
+	if (hdr_len == (*thp)->doff * 4)
+		return 0;
+
+	if (unlikely(hdr_len < (*thp)->doff * 4))
+		return -EINVAL;
+
+	parsed = *ptr - (const unsigned char *)*thp;
+
+	if (!pskb_may_pull(skb, hdr_len))
+		return -ENOMEM;
+
+	*thp = tcp_hdr(skb);
+
+	TCP_SKB_CB(skb)->end_seq = TCP_SKB_CB(skb)->seq + (*thp)->fin + skb->len - hdr_len;
+
+	*ptr = (const unsigned char *)*thp + parsed;
+	*left = hdr_len - (parsed - 2);
+
+	DEBUG_NET_WARN_ON_ONCE(get_unaligned_be16(*ptr + 2) != hdr_len);
+	DEBUG_NET_WARN_ON_ONCE(opsize == TCPOLEN_EXP_EDO_EXT_SEG &&
+			       get_unaligned_be16(*ptr + 4) != skb->len);
+
+	return 0;
+}
+
 /* Look for tcp options. Normally only called on SYN and SYNACK packets.
  * But, this can also be called on packets in the established flow when
  * the fast version below fails.
  */
 int tcp_parse_options(const struct net *net, struct sk_buff *skb,
 		      struct tcp_options_received *opt_rx, int estab,
-		      struct tcp_fastopen_cookie *foc)
+		      struct tcp_fastopen_cookie *foc, bool parse_edo_ext)
 {
 	const unsigned char *ptr;
 	const struct tcphdr *th = tcp_hdr(skb);
@@ -4237,10 +4279,27 @@ int tcp_parse_options(const struct net *net, struct sk_buff *skb,
 			case TCPOPT_EXP:
 				if (opsize >= TCPOLEN_EXP_EDO_SUPPORTED &&
 				    get_unaligned_be16(ptr) == TCPOPT_EDO_MAGIC) {
+					int ret;
+
 					if (th->syn) {
 						if (opsize == TCPOLEN_EXP_EDO_SUPPORTED)
 							opt_rx->edo_ok = 1;
+
+						/* Ignore EDO Extension in SYN and SYN+ACK. */
+						break;
 					}
+
+					if (opt_rx->edo_ok)
+						return -EINVAL;
+
+					if (!parse_edo_ext)
+						break;
+
+					ret = tcp_parse_edo_extension(skb, &th, &ptr, &length, opsize);
+					if (ret < 0)
+						return ret;
+
+					opt_rx->edo_ok = 1;
 					break;
 				}
 
@@ -4315,7 +4374,7 @@ static bool tcp_fast_parse_options(const struct net *net, struct sk_buff *skb,
 			return true;
 	}
 
-	tcp_parse_options(net, skb, &tp->rx_opt, 1, NULL);
+	tcp_parse_options(net, skb, &tp->rx_opt, 1, NULL, false);
 	if (tp->rx_opt.saw_tstamp && tp->rx_opt.rcv_tsecr)
 		tp->rx_opt.rcv_tsecr -= tp->tsoffset;
 
@@ -6325,7 +6384,7 @@ static bool tcp_rcv_fastopen_synack(struct sock *sk, struct sk_buff *synack,
 		/* Get original SYNACK MSS value if user MSS sets mss_clamp */
 		tcp_clear_options(&opt);
 		opt.user_mss = opt.mss_clamp = 0;
-		tcp_parse_options(sock_net(sk), synack, &opt, 0, NULL);
+		tcp_parse_options(sock_net(sk), synack, &opt, 0, NULL, false);
 		mss = opt.mss_clamp;
 	}
 
@@ -6409,7 +6468,7 @@ static int tcp_rcv_synsent_state_process(struct sock *sk, struct sk_buff *skb,
 	bool fastopen_fail;
 	SKB_DR(reason);
 
-	tcp_parse_options(sock_net(sk), skb, &tp->rx_opt, 0, &foc);
+	tcp_parse_options(sock_net(sk), skb, &tp->rx_opt, 0, &foc, false);
 	if (tp->rx_opt.saw_tstamp && tp->rx_opt.rcv_tsecr)
 		tp->rx_opt.rcv_tsecr -= tp->tsoffset;
 
@@ -7209,7 +7268,7 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 	tmp_opt.mss_clamp = af_ops->mss_clamp;
 	tmp_opt.user_mss  = tp->rx_opt.user_mss;
 	tcp_parse_options(sock_net(sk), skb, &tmp_opt, 0,
-			  want_cookie ? NULL : &foc);
+			  want_cookie ? NULL : &foc, false);
 
 	if (want_cookie && !tmp_opt.saw_tstamp)
 		tcp_clear_options(&tmp_opt);
