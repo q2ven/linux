8847458f4d4282a27450092b27698914f388f51a
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-08-10 22:36:39 +0000

    ipv4: Initialise ifa_preferred_lft only when ifa_valid_lft is a finite value.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/devinet.c
+++ net/ipv4/devinet.c
@@ -790,17 +790,18 @@ static void set_ifa_lifetime(struct in_ifaddr *ifa, __u32 valid_lft,
 	flags = ifa->ifa_flags & ~(IFA_F_PERMANENT | IFA_F_DEPRECATED);
 
 	timeout = addrconf_timeout_fixup(valid_lft, HZ);
-	if (addrconf_finite_timeout(timeout))
-		WRITE_ONCE(ifa->ifa_valid_lft, timeout);
-	else
-		flags |= IFA_F_PERMANENT;
-
-	timeout = addrconf_timeout_fixup(prefered_lft, HZ);
 	if (addrconf_finite_timeout(timeout)) {
+		WRITE_ONCE(ifa->ifa_valid_lft, timeout);
+
+		timeout = addrconf_timeout_fixup(prefered_lft, HZ);
 		if (timeout == 0)
 			flags |= IFA_F_DEPRECATED;
+
 		WRITE_ONCE(ifa->ifa_preferred_lft, timeout);
+	} else {
+		flags |= IFA_F_PERMANENT;
 	}
+
 	WRITE_ONCE(ifa->ifa_flags, flags);
 	WRITE_ONCE(ifa->ifa_tstamp, jiffies);
 	if (!ifa->ifa_cstamp)
