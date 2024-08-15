543800baf289fbd979a39bfd5d2f583cc5c822da
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-08-15 00:35:42 +0000

    net: loopback: Hold rtnl_net_lock() in blackhole_netdev_init().
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- drivers/net/loopback.c
+++ drivers/net/loopback.c
@@ -264,10 +264,12 @@ static int __init blackhole_netdev_init(void)
 	if (!blackhole_netdev)
 		return -ENOMEM;
 
-	rtnl_lock();
+	rtnl_lock_deprecated();
+	rtnl_net_lock(&init_net);
 	dev_init_scheduler(blackhole_netdev);
 	dev_activate(blackhole_netdev);
-	rtnl_unlock();
+	rtnl_net_unlock(&init_net);
+	rtnl_unlock_deprecated();
 
 	blackhole_netdev->flags |= IFF_UP | IFF_RUNNING;
 	dev_net_set(blackhole_netdev, &init_net);
