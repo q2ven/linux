f7e9e095d8138853513dbd59aa64be4b87c2bc50
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-12-07 07:27:47 +0000

    udp: Retire UDP-Lite for IPv6.
    Remove udplite.c and udp_impl.h.
    
    Make functions in udp_impl.h static.
    
    Include udplite.h temporarliy in udp.c for udplite_csum().
    
    Move udpv6_sendmsg() and udpv6_recvmsg() to udp.h
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- include/net/udp.h
+++ include/net/udp.h
@@ -281,6 +281,10 @@ typedef struct sock *(*udp_lookup_t)(const struct sk_buff *skb, __be16 sport,
 
 void udp_v6_early_demux(struct sk_buff *skb);
 INDIRECT_CALLABLE_DECLARE(int udpv6_rcv(struct sk_buff *));
+INDIRECT_CALLABLE_DECLARE(int udpv6_sendmsg(struct sock *, struct msghdr *,
+					    size_t));
+INDIRECT_CALLABLE_DECLARE(int udpv6_recvmsg(struct sock *, struct msghdr *,
+					    size_t, int, int *));
 
 struct sk_buff *__udp_gso_segment(struct sk_buff *gso_skb,
 				  netdev_features_t features, bool is_ipv6);
