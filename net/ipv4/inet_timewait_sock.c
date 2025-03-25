3c7d609fb190750d3c49bb6a360fbc77e7079172
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-03-25 03:59:44 +0000

    tcp: Remove timewait_sock_ops.twsk_destructor().
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/inet_timewait_sock.c
+++ net/ipv4/inet_timewait_sock.c
@@ -15,7 +15,7 @@
 #include <net/inet_hashtables.h>
 #include <net/inet_timewait_sock.h>
 #include <net/ip.h>
-
+#include <net/tcp.h>
 
 /**
  *	inet_twsk_bind_unhash - unhash a timewait socket from bind hash
@@ -73,7 +73,7 @@ static void inet_twsk_kill(struct inet_timewait_sock *tw)
 
 void inet_twsk_free(struct inet_timewait_sock *tw)
 {
-	twsk_destructor((struct sock *)tw);
+	tcp_twsk_destructor((struct sock *)tw);
 	kmem_cache_free(tw->tw_prot->twsk_prot->twsk_slab, tw);
 }
 
