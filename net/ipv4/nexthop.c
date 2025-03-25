180f861107f207e5c19ebbda6792c9175c34c2cb
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-03-25 05:51:37 +0000

    nexthop: Call alloc_skb() earlier in rtm_get_nexthop_bucket().
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv4/nexthop.c
+++ net/ipv4/nexthop.c
@@ -3833,20 +3833,23 @@ static int rtm_get_nexthop_bucket(struct sk_buff *in_skb, struct nlmsghdr *nlh,
 	if (err)
 		return err;
 
+	skb = alloc_skb(NLMSG_GOODSIZE, GFP_KERNEL);
+	if (!skb)
+		return -ENOBUFS;
+
 	nh = nexthop_find_group_resilient(net, id, extack);
-	if (IS_ERR(nh))
-		return PTR_ERR(nh);
+	if (IS_ERR(nh)) {
+		err = PTR_ERR(nh);
+		goto errout_free;
+	}
 
 	nhg = rtnl_dereference(nh->nh_grp);
 	res_table = rtnl_dereference(nhg->res_table);
 	if (bucket_index >= res_table->num_nh_buckets) {
 		NL_SET_ERR_MSG(extack, "Bucket index out of bounds");
-		return -ENOENT;
+		err = -ENOENT;
+		goto errout_free;
 	}
-
-	skb = alloc_skb(NLMSG_GOODSIZE, GFP_KERNEL);
-	if (!skb)
-		return -ENOBUFS;
 
 	err = nh_fill_res_bucket(skb, nh, &res_table->nh_buckets[bucket_index],
 				 bucket_index, RTM_NEWNEXTHOPBUCKET,
